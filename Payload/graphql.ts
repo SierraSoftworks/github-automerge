2fa163c6dbbc6f6c7a20ab55b69186db2d45f38b
Benjamin Pannell <benjamin@pannell.dev>
2021-07-13 23:22:06 +0100

    fix: More elegantly handle issues calling into GraphQL

--- Payload/graphql.ts
+++ Payload/graphql.ts
@@ -1,4 +1,4 @@
-import { asyncSpan, span } from "../utils/span";
+import { asyncSpan, span, trackException } from "../utils/span";
 import { Timer } from "../utils/timer"
 import { PullRequest } from "@octokit/webhooks-definitions/schema"
 import { graphql } from "@octokit/graphql"
@@ -9,74 +9,82 @@ import beeline = require("honeycomb-beeline");
 export class GitHubClient {
     @asyncSpan('github.enableGitHubAutoMerge', { result: '$result' })
     static async enableGitHubAutoMerge(accessToken: string, pr: PullRequest): Promise<boolean> {
-        const result = await this.callGraphQL<{
-            enablePullRequestAutoMerge?: {
-                pullRequest?: {
-                    autoMergeRequest?: {
-                        enabledAt?: string,
-                        enabledBy?: {
-                            login?: string
+        try {
+            const result = await this.callGraphQL<{
+                enablePullRequestAutoMerge?: {
+                    pullRequest?: {
+                        autoMergeRequest?: {
+                            enabledAt?: string,
+                            enabledBy?: {
+                                login?: string
+                            }
                         }
                     }
                 }
-            }
-        }>(
-            'enablePullRequestAutoMerge',
-            `
-            mutation EnableAutoMerge($pullRequest: ID!) {
-                enablePullRequestAutoMerge(input: {pullRequestId: $pullRequest}) {
-                    pullRequest {
-                        autoMergeRequest {
-                            enabledAt,
-                            enabledBy { login }
+            }>(
+                'enablePullRequestAutoMerge',
+                `
+                mutation EnableAutoMerge($pullRequest: ID!) {
+                    enablePullRequestAutoMerge(input: {pullRequestId: $pullRequest}) {
+                        pullRequest {
+                            autoMergeRequest {
+                                enabledAt,
+                                enabledBy { login }
+                            }
                         }
                     }
                 }
-            }
-            `,
-            {
-                pullRequest: pr.node_id,
-                headers: {
-                    authorization: `token ${accessToken}`
+                `,
+                {
+                    pullRequest: pr.node_id,
+                    headers: {
+                        authorization: `token ${accessToken}`
+                    }
                 }
-            }
-        )
+            )
 
-        const autoMergeResult = result?.enablePullRequestAutoMerge?.pullRequest?.autoMergeRequest
+            const autoMergeResult = result?.enablePullRequestAutoMerge?.pullRequest?.autoMergeRequest
 
-        return !!autoMergeResult?.enabledAt
+            return !!autoMergeResult?.enabledAt
+        } catch(err) {
+            trackException(err)
+        }
     }
 
     @asyncSpan('github.enableDependabotAutoMerge', { result: '$result' })
     static async enableDependabotAutoMerge(accessToken: string, pr: PullRequest): Promise<boolean> {
-        const result = await this.callGraphQL<{
-            addComment?: {
-                subject?: {
-                    id?: string
+        try {
+            const result = await this.callGraphQL<{
+                addComment?: {
+                    subject?: {
+                        id?: string
+                    }
+                }
+            }>(
+                'addComment',
+                `
+            mutation DependabotMergeComment($pullRequest: ID!, $comment: String!) {
+                addComment(input: {
+                    subjectId: $pullRequest,
+                    body: $comment
+                }) {
+                    subject { id }
                 }
             }
-        }>(
-            'addComment',
-            `
-        mutation DependabotMergeComment($pullRequest: ID!, $comment: String!) {
-            addComment(input: {
-                subjectId: $pullRequest,
-                body: $comment
-            }) {
-                subject { id }
-            }
-        }
-        `,
-            {
-                pullRequest: pr.node_id,
-                comment: "@dependabot merge",
-                headers: {
-                    authorization: `token ${accessToken}`
+            `,
+                {
+                    pullRequest: pr.node_id,
+                    comment: "@dependabot merge",
+                    headers: {
+                        authorization: `token ${accessToken}`
+                    }
                 }
-            }
-        )
+            )
 
-        return !result?.addComment?.subject?.id
+            return !result?.addComment?.subject?.id
+        } catch(err) {
+            trackException(err)
+        }
     }
 
     @asyncSpan('github.graphql', { result: '$result' })
